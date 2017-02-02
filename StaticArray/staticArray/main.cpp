f5dff03cb2ca8b4731a2f2dcf2e2d5a0a2072635
bodyo <djei0967@gmail.com>
2017-02-02 19:36:53 +0200

    add header

--- StaticArray/staticArray/main.cpp
+++ StaticArray/staticArray/main.cpp
@@ -1,62 +1,6 @@
 #include <iostream>
-#include <cassert>
-
+#include "staticarray.h"
 using namespace std;
-
-template<class data_type, size_t cap = 5>
-class Sarr
-{
-  data_type data[cap];
-public:
-  Sarr() = default;
-  explicit Sarr(const Sarr& arr)
-  {
-	for(size_t i = 0; i < cap; ++i)
-	  data[i] = arr.data[i];
-  }
-  Sarr(const data_type &a)
-  {
-	for(size_t i = 0; i < cap; ++i)
-	  data[i] = a;
-  }
-  Sarr& operator =(const Sarr& arr)
-  {
-	for(size_t i = 0; i < cap; ++i)
-	  data[i] = arr.data[i];
-	return *this;
-  }
-  Sarr& operator =(const data_type& a)=delete;
-  inline size_t size() const
-  {
-	return cap;
-  }
-  Sarr& operator +(const data_type& a);
-  Sarr& operator +(const Sarr& a);
-  data_type& operator[](const size_t index);
-};
-
-template<class data_type, size_t t>
-data_type& Sarr<data_type, t>::operator[](size_t index)
-{
-  assert(index < t);
-  return data[index];
-}
-
-template<class data_type, size_t t>
-Sarr<data_type, t>& Sarr<data_type, t>::operator +(const data_type& a)
-{
-  for(size_t i = 0; i < t; ++i)
-	data[i] += a;
-  return *this;
-}
-
-template<class data_type, size_t t>
-Sarr<data_type, t>& Sarr<data_type, t>::operator +(const Sarr& a)
-{
-  for(size_t i = 0; i < t; ++i)
-	data[i] += a.data[i];
-  return *this;
-}
 
 int main()
 {
